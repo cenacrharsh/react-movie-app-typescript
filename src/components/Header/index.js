7e95710d190da678f0ab9dd611fde15f71f861b0
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2021-07-14 20:32:59 +0530

    project setup

---
+++ src/components/Header/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import RMDBLogo from "../../images/react-movie-logo.svg";
+
+import TMDBLogo from "../../images/tmdb_logo.svg";
+
+/*Styles*/
+
+import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
+
+const Header = () => (
+  <Wrapper>
+    <Content>
+      <Link to="/">
+        <LogoImg src={RMDBLogo} alt="rmdb-logo" />
+      </Link>
+      <a href="https://www.themoviedb.org/" target="_blank" rel="noreferrer">
+        <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
+      </a>
+    </Content>
+  </Wrapper>
+);
+
+export default Header;
