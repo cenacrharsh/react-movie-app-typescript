24c2d1a16202fee2664f12a31cb29de0d178961e
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2021-07-14 22:25:20 +0530

    refactored entire app with ts

--- src/hooks/useMovieFetch.ts
+++ src/hooks/useMovieFetch.ts
@@ -1,40 +1,16 @@
 import { useState, useEffect } from "react";
-import API from "../API";
+import API, { Movie, Cast, Crew } from "../API";
 
 /*Helpers*/
 import { isPersistedState } from "../helpers";
 
-export const useMovieFetch = (movieId) => {
-  const [state, setState] = useState({});
+/*Types*/
+export type MovieState = Movie & { actors: Cast[]; directors: Crew[] };
+
+export const useMovieFetch = (movieId: string) => {
+  const [state, setState] = useState<MovieState>({} as MovieState);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
-
-  /* fetchMovie() function won't get recreated unless movieId changes, without useCallback() hook it will get recreated in each rerender and useEffect() thinks it's a new func everytime hence creating an infinite loop */
-
-  // const fetchMovie = useCallback(async () => {
-  //   try {
-  //     setLoading(true);
-  //     setError(false);
-
-  //     const movie = await API.fetchMovie(movieId);
-  //     const credits = await API.fetchCredits(movieId);
-
-  //     /*Get Directors Only*/
-  //     const directors = credits.crew.filter(
-  //       (member) => member.job === "Director"
-  //     );
-
-  //     setState({
-  //       ...movie,
-  //       actors: credits.cast,
-  //       directors,
-  //     });
-
-  //     setLoading(false);
-  //   } catch (error) {
-  //     setError(true);
-  //   }
-  // }, [movieId]);
 
   useEffect(() => {
     const fetchMovie = async () => {
